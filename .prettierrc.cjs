82ca4d25c5c897a9521e796316f4b6ea090c6772
刘三羊 <395140202@qq.com>
2024-05-27 22:13:33 +0800

    0527-02

---
+++ .prettierrc.cjs
@@ -0,0 +1,6 @@
+module.exports = {
+    // 一行最多 100 字符
+    printWidth : 100,
+    // 使用两个空格缩进
+    tabWidth : 2,
+}
---
+++ packages/types/src/.eslintrc.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+    extends:['@skimhugo/eslint-config-ts'],
+    globals:{
+        // 全局变量声明
+        // myGlobal:false
+    },
+    rules:{
+        // commom
+
+        // import
+        'import/first':'off',
+        // ts
+    },
+}
